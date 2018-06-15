1d570c8d6693c70633d0361fab8b122a9e1626ba
Michael Stoeckler <horsthacker66@gmail.com>
2018-06-15 12:45:53 +0200

    refactored

--- src/main/java/rx/RxCombineObservablesReproducer.java
+++ src/main/java/rx/RxCombineObservablesReproducer.java
@@ -1,8 +1,8 @@
 package rx;
 
 import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.observables.ConnectableObservable;
-
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,37 +11,21 @@ import java.util.concurrent.TimeUnit;
 
 public class RxCombineObservablesReproducer {
 
-    private static final int NUM_OF_OBSERVABLES = 20;
-
-    private ConnectableObservable<Data> priceObs;
-    private Observable<Data> indicator1Obs;
-    private Observable<Data> indicator2Obs;
-    private ConnectableObservable<Data> priceObsToFast;
-    private Observable<Data> indicator1ObsToFast;
-    private Observable<Data> indicator2ObsToFast;
-
     public static void main(String[] args) {
         RxCombineObservablesReproducer reproducer = new RxCombineObservablesReproducer();
-        reproducer.setUp();
         //this will work as indented
-        reproducer.combineLatestInterval();
+        reproducer.combineLatest();
         //this not
-        reproducer.combineLatestIntervalToFast();
+        reproducer.combineLatestToFast();
     }
 
-    public void setUp() {
+    private void combineLatest() {
 
-        priceObs = Observable.interval(100, TimeUnit.MILLISECONDS).map(i -> new Data("price", i * 1.00, Instant.now())).publish();
-        indicator1Obs = priceObs.map(i -> new Data("indicator1", i.getValue() * 2.00, i.getTs())).delay(10, TimeUnit.MILLISECONDS);
-        indicator2Obs = priceObs.map(i -> new Data("indicator2", i.getValue() / 2.0, i.getTs())).delay(50, TimeUnit.MILLISECONDS);
+        ConnectableObservable<Data> priceObs = Observable.interval(100, TimeUnit.MILLISECONDS).map(i -> new Data("price", i * 1.00, Instant.now())).publish();
+        Observable<Data> indicator1Obs = priceObs.map(i -> new Data("indicator1", i.getValue() * 2.00, i.getTs())).delay(10, TimeUnit.MILLISECONDS);
+        Observable<Data> indicator2Obs = priceObs.map(i -> new Data("indicator2", i.getValue() / 2.0, i.getTs())).delay(50, TimeUnit.MILLISECONDS);
 
-        priceObsToFast = Observable.interval(10, TimeUnit.MILLISECONDS).map(i -> new Data("price", i * 1.00, Instant.now())).publish();
-        indicator1ObsToFast = priceObsToFast.map(i -> new Data("indicator1ToFast", i.getValue() * 2.00, i.getTs())).delay(10, TimeUnit.MILLISECONDS);
-        indicator2ObsToFast = priceObsToFast.map(i -> new Data("indicator2ToFast", i.getValue() / 2.00, i.getTs())).delay(50, TimeUnit.MILLISECONDS);
-    }
-
-    private void combineLatestInterval() {
-        Observable.combineLatest(priceObs, indicator1Obs, indicator2Obs, (Data price, Data indicator1, Data indicator2) -> {
+        Disposable dis = Observable.combineLatest(priceObs, indicator1Obs, indicator2Obs, (Data price, Data indicator1, Data indicator2) -> {
                     if (checkSameTs(price, indicator1, indicator2)) {
                         Map result = new HashMap<String, Data>();
                         result.put("price", price);
@@ -57,18 +41,24 @@ public class RxCombineObservablesReproducer {
                 .subscribe(System.out::println);
         priceObs.connect();
         sleep(1000);
+        dis.dispose();
     }
 
-    private void combineLatestIntervalToFast() {
+    private void combineLatestToFast() {
+
+        ConnectableObservable<Data> priceObsToFast = Observable.interval(100, TimeUnit.MILLISECONDS).map(i -> new Data("price", i * 1.00, Instant.now())).publish();
+        Observable<Data> indicator1ObsToFast = priceObsToFast.map(i -> new Data("indicator1ToFast", i.getValue() * 2.00, i.getTs())).delay(9, TimeUnit.MILLISECONDS);
+        Observable<Data> indicator2ObsToFast = priceObsToFast.map(i -> new Data("indicator2ToFast", i.getValue() / 2.00, i.getTs())).delay(15, TimeUnit.MILLISECONDS);
+
         System.out.println("\n------------------------------toFast------------------------------\n");
-        Observable.combineLatest(priceObsToFast, indicator1ObsToFast, indicator2ObsToFast, (Data priceToFast, Data indicator1ToFast, Data indicator2ToFast) -> {
-            if (checkSameTs(priceToFast, indicator1ToFast, indicator2ToFast)) {
-                Map result = new HashMap<String, Data>();
-                System.out.println("combineToFast");
-                result.put("priceToFast", priceToFast);
-                result.put("indicator1ToFast", indicator1ToFast);
-                result.put("indicator2ToFast", indicator2ToFast);
-                return result;
+        Disposable dis = Observable.combineLatest(priceObsToFast, indicator1ObsToFast, indicator2ObsToFast, (Data priceToFast, Data indicator1ToFast, Data indicator2ToFast) -> {
+                    //System.out.println("combineToFast");
+                    if (checkSameTs(priceToFast, indicator1ToFast, indicator2ToFast)) {
+                        Map result = new HashMap<String, Data>();
+                        result.put("priceToFast", priceToFast);
+                        result.put("indicator1ToFast", indicator1ToFast);
+                        result.put("indicator2ToFast", indicator2ToFast);
+                        return result;
                     }
                     return new HashMap<String, Data>();
                 }
@@ -78,6 +68,7 @@ public class RxCombineObservablesReproducer {
                 .subscribe(System.out::println);
         priceObsToFast.connect();
         sleep(1000);
+        dis.dispose();
     }
 
     private boolean checkSameTs(Data... dataObjects) {
